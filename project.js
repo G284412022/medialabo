b46b2c64e0e66303d7ccab29fb00d7ea8adf63a8
G284422022 <228441@sa.takushoku-u.ac.jp>
2023-06-07 13:25:40 +0900

    c

--- project.js
+++ project.js
@@ -50,3 +50,13 @@ let data = {
 cosole.log(data.name+","+data.main.temp_max+","+data.main.temp_min);
 p=document.createElement('P');
 p.textContent=data.nemu+","+data.main.temp_max+","+data.main.temp_min;
+
+let b = document.querySelector('#print');
+b.addEventListener('click', greeting);
+
+// 2. イベントハンドラの定義
+function greeting() {
+    let i = document.querySelector('input[name="shimei"]');
+    let shimei = i.value;       // ユーザが記入した文字列
+    console.log('こんにちは! ' + shimei + ' さん');
+}
